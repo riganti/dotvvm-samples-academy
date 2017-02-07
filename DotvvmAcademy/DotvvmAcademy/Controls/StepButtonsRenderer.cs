67b777759b57113da432b0aaf0a89d9157e8dd39
Patrik Švikruha <patrik.svikruha@gmail.com>
2017-02-07 15:34:07 +0100

    Fix broken unique ID

--- DotvvmAcademy/DotvvmAcademy/Controls/StepButtonsRenderer.cs
+++ DotvvmAcademy/DotvvmAcademy/Controls/StepButtonsRenderer.cs
@@ -6,24 +6,25 @@ using DotVVM.Framework.Hosting;
 
 namespace DotvvmAcademy.Controls
 {
-    public class StepButtonsRenderer : HtmlGenericControl
-    {
-        public StepButtonsRenderer() : base("div")
-        {
-        }
+	public class StepButtonsRenderer : HtmlGenericControl
+	{
+		public StepButtonsRenderer() : base("div")
+		{
+		}
 
-        protected override void OnInit(IDotvvmRequestContext context)
-        {
-            var controlBuilderFactory = context.Configuration.ServiceLocator.GetService<IControlBuilderFactory>();
+		protected override void OnInit(IDotvvmRequestContext context)
+		{
+			var controlBuilderFactory = context.Configuration.ServiceLocator.GetService<IControlBuilderFactory>();
 
-            if (DataContext is CodeStepBase<IDotHtmlCodeValidationObject> ||
-                DataContext is CodeStepBase<ICSharpCodeValidationObject>)
-            {
-                var builder = controlBuilderFactory.GetControlBuilder("Controls/CodeEditorButtons.dotcontrol");
-                var control = builder.BuildControl(controlBuilderFactory);
-                Children.Add(control);
-            }
-            base.OnInit(context);
-        }
-    }
+			if (DataContext is CodeStepBase<IDotHtmlCodeValidationObject> ||
+			    DataContext is CodeStepBase<ICSharpCodeValidationObject>)
+			{
+				var builder = controlBuilderFactory.GetControlBuilder("Controls/CodeEditorButtons.dotcontrol");
+				var control = builder.BuildControl(controlBuilderFactory);
+				control.SetValue(Internal.UniqueIDProperty, "c1");
+				Children.Add(control);
+			}
+			base.OnInit(context);
+		}
+	}
 }
--- DotvvmAcademy/DotvvmAcademy/ViewModels/LessonViewModel.cs
+++ DotvvmAcademy/DotvvmAcademy/ViewModels/LessonViewModel.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using DotvvmAcademy.Cache;
@@ -11,92 +10,77 @@ using DotVVM.Framework.Hosting;
 
 namespace DotvvmAcademy.ViewModels
 {
-    public class LessonViewModel : SiteViewModel
-    {
-        protected LessonBase lesson;
-        protected int lessonNumber;
-        public IStep Step { get; set; }
-        public string ErrorMessage { get; private set; }
-        public bool ContinueButtonVisible { get; set; } = true;
-        protected int CurrentStepNumber { get; set; }
+	public class LessonViewModel : SiteViewModel
+	{
+		protected LessonBase lesson;
+		protected int lessonNumber;
+		public IStep Step { get; set; }
+		public string ErrorMessage { get; private set; }
+		public bool ContinueButtonVisible { get; set; } = true;
+		protected int CurrentStepNumber { get; set; }
 
-        public override Task Init()
-        {
-            LoadStep();
+		public override Task Init()
+		{
+			LoadStep();
 
-            return base.Init();
-        }
+			return base.Init();
+		}
 
-        private void LoadStep()
-        {
-            lessonNumber = Convert.ToInt32(Context.Parameters["Lesson"]);
-            CurrentStepNumber = Convert.ToInt32(Context.Parameters["Step"]);
+		private void LoadStep()
+		{
+			lessonNumber = Convert.ToInt32(Context.Parameters["Lesson"]);
+			CurrentStepNumber = Convert.ToInt32(Context.Parameters["Step"]);
 
-            var cache = new LessonsCache();
-            var lessons = cache.Get();
+			var cache = new LessonsCache();
+			var lessons = cache.Get();
 
 
-            if (lessonNumber == 1)
-            {
-                lesson = lessons.First(l => l.Key == 1).Value;
-            }
-            else if (lessonNumber == 2)
-            {
-                lesson = lessons.First(l => l.Key == 2).Value;
-            }
-            else if (lessonNumber == 3)
-            {
-                lesson = lessons.First(l => l.Key == 3).Value;
-            }
-            else if (lessonNumber == 4)
-            {
-                lesson = lessons.First(l => l.Key == 4).Value;
-            }
-            else
-            {
-                throw new NotSupportedException();
-            }
-            Step = lesson.Steps.First(s => s.StepIndex == CurrentStepNumber);
-            AfterLoad();
-        }
+			if (lessonNumber == 1)
+				lesson = lessons.First(l => l.Key == 1).Value;
+			else if (lessonNumber == 2)
+				lesson = lessons.First(l => l.Key == 2).Value;
+			else if (lessonNumber == 3)
+				lesson = lessons.First(l => l.Key == 3).Value;
+			else if (lessonNumber == 4)
+				lesson = lessons.First(l => l.Key == 4).Value;
+			else
+				throw new NotSupportedException();
+			Step = lesson.Steps.First(s => s.StepIndex == CurrentStepNumber);
+			AfterLoad();
+		}
 
-        protected virtual void AfterLoad()
-        {
-        }
+		protected virtual void AfterLoad()
+		{
+		}
 
-        public void Continue()
-        {
-            var storage = new LessonProgressStorage(Context.GetAspNetCoreContext());
+		public void Continue()
+		{
+			var storage = new LessonProgressStorage(Context.GetAspNetCoreContext());
 
-            var step = Step as ValidableStepBase;
-            if (step != null)
-            {
-                var validableStep = step;
-                if (Context.IsPostBack)
-                {
-                   ErrorMessage = validableStep.Validate();
-                }
-               
-            }
+			var step = Step as ValidableStepBase;
+			if (step != null)
+			{
+				var validableStep = step;
+				if (Context.IsPostBack)
+					ErrorMessage = validableStep.Validate();
+			}
 
-            if (string.IsNullOrEmpty(ErrorMessage))
-            {
-                if (CurrentStepNumber < lesson.Steps.Count())
-                {
-                    storage.UpdateLessonLastStep(lessonNumber, CurrentStepNumber + 1);
-                    RedirectToNextLesson();
-                }
-                else
-                {
-                    storage.UpdateLessonLastStep(lessonNumber, LessonProgressStorage.FinishedLessonStepNumber);
-                    Context.RedirectToRoute("Default");
-                }
-            }
-        }
+			if (string.IsNullOrEmpty(ErrorMessage))
+				if (CurrentStepNumber < lesson.Steps.Count())
+				{
+					storage.UpdateLessonLastStep(lessonNumber, CurrentStepNumber + 1);
+					RedirectToNextLesson();
+				}
+				else
+				{
+					storage.UpdateLessonLastStep(lessonNumber, LessonProgressStorage.FinishedLessonStepNumber);
+					Context.RedirectToRoute("Default");
+				}
+		}
 
-        protected virtual void RedirectToNextLesson()
-        {
-            Context.RedirectToRoute("Lesson", new {Step = CurrentStepNumber + 1});
-        }
-    }
+		protected virtual void RedirectToNextLesson()
+		{
+			Context.RedirectToRoute("Lesson", new {Step = CurrentStepNumber + 1});
+		}
+	}
 }
