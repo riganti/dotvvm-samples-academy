85ec0f6661e64f973e96a6f55616ebf1c2cc7a86
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-01-02 19:58:24 +0100

    Aesthetic changes to Startup

--- DotvvmAcademy/DotvvmAcademy/Startup.cs
+++ DotvvmAcademy/DotvvmAcademy/Startup.cs
@@ -32,10 +32,8 @@ namespace DotvvmAcademy
         {
             builder.RegisterModule<BLModule>();
             builder.RegisterType<AutoMapperInitializer>();
-
             builder.RegisterAssemblyTypes(typeof(Startup).Assembly)
                 .AsClosedTypesOf(typeof(StepPartRenderer<>));
-
             builder.RegisterAssemblyTypes(typeof(Startup).Assembly)
                 .AssignableTo<IDotvvmViewModel>()
                 .AsSelf();
