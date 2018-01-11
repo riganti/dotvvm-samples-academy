61a28d9a7c542d1797ba57a0815edbefdb41c74f
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-01-11 16:43:53 +0100

    Add docstring for ValidationDiagnostic

--- DotvvmAcademy/DotvvmAcademy.Validation/ValidationDiagnostic.cs
+++ DotvvmAcademy/DotvvmAcademy.Validation/ValidationDiagnostic.cs
@@ -1,7 +1,16 @@
 ﻿namespace DotvvmAcademy.Validation
 {
+    /// <summary>
+    /// Represents an issue found by an <see cref="IValidationService{TRequest, TResponse}"/>.
+    /// </summary>
     public class ValidationDiagnostic
     {
+        /// <summary>
+        /// Creates a new <see cref="ValidationDiagnostic"/>.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="message"></param>
+        /// <param name="location"></param>
         public ValidationDiagnostic(string id, string message, ValidationDiagnosticLocation location)
         {
             Id = id;
@@ -9,10 +18,19 @@
             Location = location;
         }
 
+        /// <summary>
+        /// The unique identification code of the found issue.
+        /// </summary>
         public string Id { get; set; }
 
+        /// <summary>
+        /// The specific location in a file of the found issue.
+        /// </summary>
         public ValidationDiagnosticLocation Location { get; set; }
 
+        /// <summary>
+        /// The message describing the found issue to the user.
+        /// </summary>
         public string Message { get; set; }
     }
 }
