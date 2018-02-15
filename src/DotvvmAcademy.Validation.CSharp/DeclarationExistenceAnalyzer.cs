abd6f1ec6676539961b1293c8f1df56853aaf95a
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-02-15 10:53:45 +0100

    Add DeclarationExistenceAnalyzer and its tests

---
+++ src/DotvvmAcademy.Validation.CSharp/DeclarationExistenceAnalyzer.cs
@@ -0,0 +1,63 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace DotvvmAcademy.Validation.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DeclarationExistenceAnalyzer : ValidationDiagnosticAnalyzer
+    {
+        public const string MetadataKey = "DoesDeclarationExist";
+
+        public static readonly DiagnosticDescriptor DeclarationForbiddenDiagnostic = new DiagnosticDescriptor(
+            id: "TEMP01",
+            title: "Declaration Forbidden",
+            messageFormat: "A declaration for symbol {0} is forbidden.",
+            category: "Temporary",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true);
+
+        public static readonly DiagnosticDescriptor DeclarationRequiredDiagnostic = new DiagnosticDescriptor(
+            id: "TEMP02",
+            title: "Declaration Missing",
+            messageFormat: "A declaration for symbol '{0}' is required.",
+            category: "Temporary",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true);
+
+        private ImmutableArray<MetadataName> names;
+
+        public DeclarationExistenceAnalyzer(MetadataCollection metadata) : base(metadata)
+        {
+            names = metadata.GetNamesWithProperty(MetadataKey).ToImmutableArray();
+        }
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+            = ImmutableArray.Create(DeclarationRequiredDiagnostic, DeclarationForbiddenDiagnostic);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterCompilationAction(ValidateCompilation);
+        }
+
+        private void ValidateCompilation(CompilationAnalysisContext context)
+        {
+            foreach (var name in names)
+            {
+                var doesDeclarationExist = Metadata.RequireProperty<bool>(name, MetadataKey);
+                var symbolExists = TryGetSymbol(context.Compilation, name, out var symbol);
+                if (doesDeclarationExist && !symbolExists)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(DeclarationRequiredDiagnostic, Location.None, name.FullName));
+                }
+                else if (!doesDeclarationExist && symbolExists)
+                {
+                    foreach (var location in symbol.Locations)
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(DeclarationForbiddenDiagnostic, location, name.FullName));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/DotvvmAcademy.Validation.Tests/DeclarationExistenceAnalyzerTests.cs
@@ -0,0 +1,50 @@
+﻿using DotvvmAcademy.Validation.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace DotvvmAcademy.Validation.Tests
+{
+    [TestClass]
+    public class DeclarationExistenceAnalyzerTests : AnalyzerTestBase
+    {
+        public const string BasicSample = @"
+namespace SampleNamespace
+{
+    public class SampleClass
+    {
+        public int IntegerProperty { get; set; }
+
+        public string GetString()
+        {
+            return IntegerProperty.ToString() + ObjectProperty.ToString();
+        }
+    }
+}";
+
+        private MetadataName sampleClass;
+        private MetadataName integerProperty;
+        private MetadataName getString;
+
+        public DeclarationExistenceAnalyzerTests()
+        {
+            var int32 = MetadataName.CreateTypeName("System", "Int32");
+            var @string = MetadataName.CreateTypeName("System", "String");
+
+            sampleClass = MetadataName.CreateTypeName("SampleNamespace", "SampleClass");
+            integerProperty = MetadataName.CreatePropertyName(int32, sampleClass, "IntegerProperty");
+            getString = MetadataName.CreateMethodName(sampleClass, "GetString", @string);
+        }
+
+        [TestMethod]
+        public async Task BasicDeclarationExistenceAnalyzerTest()
+        {
+            var metadata = new MetadataCollection();
+            metadata[sampleClass, DeclarationExistenceAnalyzer.MetadataKey] = true;
+            metadata[integerProperty, DeclarationExistenceAnalyzer.MetadataKey] = true;
+            metadata[getString, DeclarationExistenceAnalyzer.MetadataKey] = false;
+
+            var analyzer = new DeclarationExistenceAnalyzer(metadata);
+            var results = await TestAnalyzer(analyzer, BasicSample);
+        }
+    }
+}
