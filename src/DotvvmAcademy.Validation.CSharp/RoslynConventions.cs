6f6540b3088638bda9f4a26a6b67b3679d58a74e
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-02-15 10:44:36 +0100

    Replace SymbolKind and SyntaxKind presets with RoslynConventions

---
+++ src/DotvvmAcademy.Validation.CSharp/RoslynConventions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Immutable;
+
+namespace DotvvmAcademy.Validation.CSharp
+{
+    public static class RoslynConventions
+    {
+        public static readonly ImmutableArray<SymbolKind> AllSymbols = ImmutableArray.Create(
+            SymbolKind.Alias,
+            SymbolKind.ArrayType,
+            SymbolKind.Assembly,
+            SymbolKind.DynamicType,
+            SymbolKind.ErrorType,
+            SymbolKind.Event,
+            SymbolKind.Field,
+            SymbolKind.Label,
+            SymbolKind.Local,
+            SymbolKind.Method,
+            SymbolKind.NetModule,
+            SymbolKind.NamedType,
+            SymbolKind.Namespace,
+            SymbolKind.Parameter,
+            SymbolKind.PointerType,
+            SymbolKind.Property,
+            SymbolKind.RangeVariable,
+            SymbolKind.TypeParameter,
+            SymbolKind.Preprocessing,
+            SymbolKind.Discard);
+
+        public static readonly ImmutableArray<SyntaxKind> Declarations = ImmutableArray.Create(
+            SyntaxKind.NamespaceDeclaration,
+            SyntaxKind.ClassDeclaration,
+            SyntaxKind.StructDeclaration,
+            SyntaxKind.InterfaceDeclaration,
+            SyntaxKind.EnumDeclaration,
+            SyntaxKind.DelegateDeclaration,
+            SyntaxKind.EnumMemberDeclaration,
+            SyntaxKind.FieldDeclaration,
+            SyntaxKind.EventFieldDeclaration,
+            SyntaxKind.MethodDeclaration,
+            SyntaxKind.OperatorDeclaration,
+            SyntaxKind.ConversionOperatorDeclaration,
+            SyntaxKind.ConstructorDeclaration,
+            SyntaxKind.DestructorDeclaration,
+            SyntaxKind.PropertyDeclaration,
+            SyntaxKind.EventDeclaration,
+            SyntaxKind.IndexerDeclaration,
+            SyntaxKind.GetAccessorDeclaration,
+            SyntaxKind.SetAccessorDeclaration,
+            SyntaxKind.AddAccessorDeclaration,
+            SyntaxKind.RemoveAccessorDeclaration,
+            SyntaxKind.UnknownAccessorDeclaration);
+
+        public static readonly ImmutableArray<SyntaxKind> Identifiers = ImmutableArray.Create(
+            SyntaxKind.PredefinedType,
+            SyntaxKind.IdentifierName);
+    }
+}
