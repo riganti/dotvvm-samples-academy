c108b67c202e90962e765f17d8774eb9e5fbca1a
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-02-15 20:37:24 +0100

    Add SymbolAllowedAnalyzer and its tests class

---
+++ src/DotvvmAcademy.Validation.CSharp/SymbolAllowedAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace DotvvmAcademy.Validation.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SymbolAllowedAnalyzer : ValidationDiagnosticAnalyzer
+    {
+        public const string MetadataKey = "IsAllowed";
+
+        public static readonly DiagnosticDescriptor SymbolUsageForbiddenDiagnostic = new DiagnosticDescriptor(
+            id: "TEMP03",
+            title: "Symbol Usage Forbidden",
+            messageFormat: "Usage of symbol {0} is forbidden.",
+            category: "Temporary",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true);
+
+        private readonly RoslynMetadataNameProvider nameProvider;
+
+        public SymbolAllowedAnalyzer(MetadataCollection metadata, RoslynMetadataNameProvider nameProvider) : base(metadata)
+        {
+            this.nameProvider = nameProvider;
+        }
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+            = ImmutableArray.Create(SymbolUsageForbiddenDiagnostic);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(ValidateNode, RoslynConventions.Identifiers);
+        }
+
+        public void ValidateNode(SyntaxNodeAnalysisContext context)
+        {
+            var symbol = context.SemanticModel.GetSymbolInfo(context.Node).Symbol;
+            if (!MetadataName.IsSupportedSymbol(symbol))
+            {
+                return;
+            }
+
+            var symbolName = nameProvider.GetName(symbol);
+            if (!true.Equals(Metadata.GetProperty(symbolName, MetadataKey)))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(SymbolUsageForbiddenDiagnostic, context.Node.GetLocation(), symbolName));
+            }
+        }
+    }
+}
---
+++ test/DotvvmAcademy.Validation.CSharp.Tests/SymbolAllowedAnalyzerTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace DotvvmAcademy.Validation.CSharp.Tests
+{
+    [TestClass]
+    public class SymbolAllowedAnalyzerTests : AnalyzerTestBase
+    {
+        public const string Sample = @"
+public class TestClass
+{
+    public void TestMethod(string s)
+    {
+        s.Clone();
+        s.CompareTo("""");
+        s.Contains(""Test"");
+        s.EndsWith(""Test"");
+    }
+}";
+
+        [TestMethod]
+        public async Task BasicSymbolAllowedAnalyzerTest()
+        {
+            var @void = MetadataName.CreateTypeName("System", "Void");
+            var @object = MetadataName.CreateTypeName("System", "Object");
+            var @int = MetadataName.CreateTypeName("System", "Int32");
+            var @string = MetadataName.CreateTypeName("System", "String");
+            var @bool = MetadataName.CreateTypeName("System", "Bool");
+
+            var clone = MetadataName.CreateMethodName(@string, "Clone", @object);
+            var compareTo = MetadataName.CreateMethodName(@string, "CompareTo", @int, ImmutableArray.Create(@string));
+            var contains = MetadataName.CreateMethodName(@string, "Contains", @bool, ImmutableArray.Create(@string));
+            var endsWith = MetadataName.CreateMethodName(@string, "EndsWith", @bool, ImmutableArray.Create(@string));
+
+            var metadata = new MetadataCollection();
+            metadata[clone, SymbolAllowedAnalyzer.MetadataKey] = true;
+            metadata[compareTo, SymbolAllowedAnalyzer.MetadataKey] = true;
+            metadata[@string, SymbolAllowedAnalyzer.MetadataKey] = true;
+            metadata[@void, SymbolAllowedAnalyzer.MetadataKey] = true;
+            metadata[contains, SymbolAllowedAnalyzer.MetadataKey] = false;
+
+            var nameProvider = new RoslynMetadataNameProvider();
+            var analyzer = new SymbolAllowedAnalyzer(metadata, nameProvider);
+            var compilation = GetCompilation(Sample);
+            var result = await TestAnalyzer(compilation, analyzer);
+
+            Assert.AreEqual(2, result.Length);
+        }
+    }
+}
