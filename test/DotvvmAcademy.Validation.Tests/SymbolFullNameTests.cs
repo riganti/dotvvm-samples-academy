1bb31bb405242600ae6cb92dea869e26d6bbe70b
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-02-15 10:51:54 +0100

    Add SymbolFullNameTests

---
+++ test/DotvvmAcademy.Validation.Tests/SymbolFullNameTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DotvvmAcademy.Validation.Tests
+{
+    [TestClass]
+    public class SymbolFullNameTests : CSharpTestBase
+    {
+        public const string SampleResourceName = "DotvvmAcademy.Validation.Tests.ShopSample.cs";
+
+        public string Sample { get; set; }
+
+        [TestMethod]
+        public void BasicSymbolFullNameTest()
+        {
+            var compilation = GetCompilation(Sample, "Shop", new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+            compilation = compilation.AddReferences(GetMetadataReference("System.Linq"));
+            var roslyn = new NameAggregationRoslynVisitor();
+            roslyn.Visit(compilation.Assembly);
+            var definition = GetAssemblyDefinition(compilation);
+            var cecil = new NameAggregationCecilVisitor();
+            cecil.Visit(definition);
+            var assembly = GetAssembly(compilation);
+            var reflection = new NameAggregationReflectionVisitor();
+            reflection.Visit(assembly);
+
+            SerializeFullNames(roslyn.FullNames, "roslyn.txt");
+            SerializeFullNames(cecil.FullNames, "cecil.txt");
+            SerializeFullNames(reflection.FullNames, "reflection.txt");
+        }
+
+        [TestInitialize]
+        public void LoadSample()
+        {
+            var assembly = typeof(SymbolFullNameTests).Assembly;
+            var stream = assembly.GetManifestResourceStream(SampleResourceName);
+            using (var sr = new StreamReader(stream))
+            {
+                Sample = sr.ReadToEnd();
+            }
+        }
+
+        private void SerializeFullNames(List<string> fullNames, string fileName)
+        {
+            var directory = Directory.GetCurrentDirectory();
+            using (var writer = new StreamWriter(Path.Combine(directory, fileName)))
+            {
+                foreach (var fullName in fullNames)
+                {
+                    writer.WriteLine(fullName);
+                }
+            }
+        }
+    }
+}
