e540b7369c7fc28766b52338336bdaecbb7422d3
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-02-16 10:58:39 +0100

    Add InterfaceImplementationAnalyzer and related classes

---
+++ src/DotvvmAcademy.Validation.CSharp/InterfaceImplementationAnalyzer.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace DotvvmAcademy.Validation.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class InterfaceImplementationAnalyzer : ValidationDiagnosticAnalyzer
+    {
+        public const string NegativeMetadataKey = "IsNotImplementing";
+        public const string PositiveMetadataKey = "IsImplementing";
+
+        public static readonly DiagnosticDescriptor ForbiddenInterfaceImplementationDiagnostic = new DiagnosticDescriptor(
+            id: "TEMP09",
+            title: "Forbidden Interface Implementation",
+            messageFormat: "The type {0} is forbidden from implementing interface {1}.",
+            category: "Temporary",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true);
+
+        public static readonly DiagnosticDescriptor MissingInterfaceImplementationDiagnostic = new DiagnosticDescriptor(
+            id: "TEMP10",
+            title: "Missing Interface Implementation",
+            messageFormat: "The type {0} must implement interface {1}.",
+            category: "Temporary",
+            defaultSeverity: DiagnosticSeverity.Error,
+            isEnabledByDefault: true);
+
+        private readonly SymbolLocator locator;
+        private readonly ImmutableArray<MetadataName> negativeNames;
+        private readonly ImmutableArray<MetadataName> positiveNames;
+
+        public InterfaceImplementationAnalyzer(MetadataCollection metadata, SymbolLocator locator) : base(metadata)
+        {
+            positiveNames = metadata.GetNamesWithProperty(PositiveMetadataKey).ToImmutableArray();
+            negativeNames = metadata.GetNamesWithProperty(NegativeMetadataKey).ToImmutableArray();
+            this.locator = locator;
+        }
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+            = ImmutableArray.Create(ForbiddenInterfaceImplementationDiagnostic, MissingInterfaceImplementationDiagnostic);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterCompilationAction((c) => Validate(
+                context: c,
+                names: positiveNames,
+                metadataKey: PositiveMetadataKey,
+                descriptor: MissingInterfaceImplementationDiagnostic,
+                reportOnContains: false));
+            context.RegisterCompilationAction((c) => Validate(
+                context: c,
+                names: negativeNames,
+                metadataKey: NegativeMetadataKey,
+                descriptor: ForbiddenInterfaceImplementationDiagnostic,
+                reportOnContains: true));
+        }
+
+        private void Validate(CompilationAnalysisContext context, ImmutableArray<MetadataName> names, string metadataKey,
+            DiagnosticDescriptor descriptor, bool reportOnContains)
+        {
+            foreach (var name in names)
+            {
+                if (locator.TryGetSymbol(name, out var symbol)
+                    && symbol is ITypeSymbol typeSymbol)
+                {
+                    var interfaceNames = Metadata.RequireProperty<ImmutableArray<MetadataName>>(name, metadataKey);
+                    foreach (var interfaceName in interfaceNames)
+                    {
+                        if (locator.TryGetSymbol(interfaceName, out var possibleInterfaceSymbol)
+                            && possibleInterfaceSymbol is INamedTypeSymbol interfaceSymbol
+                            && typeSymbol.Interfaces.Contains(interfaceSymbol) == reportOnContains)
+                        {
+                            foreach (var location in typeSymbol.Locations)
+                            {
+                                context.ReportDiagnostic(Diagnostic.Create(descriptor, location, name, interfaceName));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/DotvvmAcademy.Validation.CSharp.Tests/InterfaceImplementationAnalyzerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotvvmAcademy.Validation.CSharp.Tests
+{
+    [TestClass]
+    public class InterfaceImplementationAnalyzerTests : AnalyzerTestBase
+    {
+        public const string Sample = @"
+public interface FirstInterface
+{
+}
+
+public interface SecondInterface
+{
+}
+
+public class FirstClass : FirstInterface
+{
+}
+
+public class SecondClass : FirstInterface
+{
+}
+
+public class ThirdClass : FirstInterface, SecondInterface
+{
+}
+";
+
+        [TestMethod]
+        public async Task BasicInterfaceImplementationAnalyzerTest()
+        {
+            var firstInterface = MetadataName.CreateTypeName("", "FirstInterface");
+            var secondInterface = MetadataName.CreateTypeName("", "SecondInterface");
+            var firstClass = MetadataName.CreateTypeName("", "FirstClass");
+            var secondClass = MetadataName.CreateTypeName("", "SecondClass");
+            var thirdClass = MetadataName.CreateTypeName("", "ThirdClass");
+
+            var metadata = new MetadataCollection();
+            metadata[firstClass, InterfaceImplementationAnalyzer.PositiveMetadataKey] = ImmutableArray.Create(firstInterface);
+            metadata[secondClass, InterfaceImplementationAnalyzer.PositiveMetadataKey] = ImmutableArray.Create(secondInterface);
+            metadata[secondClass, InterfaceImplementationAnalyzer.NegativeMetadataKey] = ImmutableArray.Create(firstInterface);
+            metadata[thirdClass, InterfaceImplementationAnalyzer.PositiveMetadataKey] = ImmutableArray.Create(firstInterface, secondInterface);
+
+            var compilation = GetCompilation(Sample);
+            var nameProvider = new RoslynMetadataNameProvider();
+            var locator = new SymbolLocator(compilation, nameProvider);
+            var analyzer = new InterfaceImplementationAnalyzer(metadata, locator);
+
+            var result = await TestAnalyzer(compilation, analyzer);
+            Assert.AreEqual(2, result.Length);
+        }
+    }
+}
