c6da4f1425da7e432f31a12bbb89400317f6658b
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-03-02 17:21:40 +0100

    Add IValidationService prototype api

---
+++ src/DotvvmAcademy.Validation/IValidationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace DotvvmAcademy.Validation
+{
+    public interface IValidationService
+    {
+        Task Validate(ValidationContext context);
+    }
+}
---
+++ src/DotvvmAcademy.Validation/ValidationContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+
+namespace DotvvmAcademy.Validation
+{
+    public class ValidationContext
+    {
+        private ConcurrentDictionary<string, object> items;
+
+        public object GetItem(string key)
+        {
+            if (items.TryGetValue(key, out var value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+
+        public void SetItem(string key, object item)
+        {
+            items.AddOrUpdate(key, item, (k, o) => item);
+        }
+    }
+}
