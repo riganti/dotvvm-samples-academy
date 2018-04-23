be2bf5297e0d48fbdd3ec38647d99b7d93330533
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-04-23 08:40:55 +0200

    Add ErrorAggregatingVisitor

---
+++ src/DotvvmAcademy.Validation.Dothtml/DothtmlValidationDiagnostic.cs
@@ -0,0 +1,29 @@
+﻿using DotVVM.Framework.Compilation.Parser.Dothtml.Parser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotvvmAcademy.Validation.Dothtml
+{
+    internal class DothtmlValidationDiagnostic : ValidationDiagnostic
+    {
+        private readonly DothtmlNode node;
+
+        public DothtmlValidationDiagnostic(DothtmlNode node, string message)
+        {
+            this.node = node;
+            Message = message;
+            Location = new DothtmlValidationDiagnosticLocation(node);
+        }
+
+        public override string Id { get; } = "DOTHTML";
+
+        public override ValidationDiagnosticLocation Location { get; }
+
+        public override string Message { get; }
+
+        public override string Name { get; } = "Dothtml Node Error";
+
+        public override ValidationDiagnosticSeverity Severity { get; } = ValidationDiagnosticSeverity.Error;
+    }
+}
---
+++ src/DotvvmAcademy.Validation.Dothtml/ErrorAggregatingVisitor.cs
@@ -0,0 +1,58 @@
+﻿using DotVVM.Framework.Compilation.ControlTree.Resolved;
+using DotVVM.Framework.Compilation.Parser.Dothtml.Parser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotvvmAcademy.Validation.Dothtml
+{
+    public class ErrorAggregatingVisitor : ResolvedControlTreeVisitor
+    {
+        public List<ValidationDiagnostic> Errors { get; } = new List<ValidationDiagnostic>();
+
+        public override void VisitControl(ResolvedControl control)
+        {
+            if (control.DothtmlNode.HasNodeErrors)
+            {
+                AddErrors(control.DothtmlNode);
+            }
+            base.VisitControl(control);
+        }
+
+        public override void VisitView(ResolvedTreeRoot view)
+        {
+            if (view.DothtmlNode.HasNodeErrors)
+            {
+                AddErrors(view.DothtmlNode);
+            }
+            foreach (var directive in ((DothtmlRootNode)view.DothtmlNode).Directives)
+            {
+                if (directive.HasNodeErrors)
+                {
+                    AddErrors(directive);
+                }
+            }
+            base.VisitView(view);
+        }
+
+        public override void VisitPropertyBinding(ResolvedPropertyBinding propertyBinding)
+        {
+            var errors = propertyBinding.Binding.Errors;
+            if (errors.HasErrors)
+            {
+                foreach(var exception in errors.Exceptions)
+                {
+                    Errors.Add(new DothtmlValidationDiagnostic(propertyBinding.DothtmlNode, exception.Message));
+                }
+            }
+        }
+
+        private void AddErrors(DothtmlNode node)
+        {
+            foreach (var error in node.NodeErrors)
+            {
+                Errors.Add(new DothtmlValidationDiagnostic(node, error));
+            }
+        }
+    }
+}
