a7687bbc9cacbdf82f2672d29770754ceb4645c4
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-04-30 13:27:43 +0200

    Add GetProperty to MetadataCollectionExtensions

--- src/DotvvmAcademy.Validation/MetadataCollectionExtensions.cs
+++ src/DotvvmAcademy.Validation/MetadataCollectionExtensions.cs
@@ -6,19 +6,24 @@ namespace DotvvmAcademy.Validation
 {
     public static class MetadataCollectionExtensions
     {
-        public static TProperty GetRequiredProperty<TIdentifier, TProperty>(this IMetadataCollection<TIdentifier> collection, TIdentifier identifier, string key)
+        public static TProperty GetProperty<TIdentifier, TProperty>(this IMetadataCollection<TIdentifier> collection, TIdentifier identifier, string key)
         {
             var value = collection[identifier, key];
-            if (value == null)
-            {
-                throw new InvalidOperationException($"The value of the '{key}' property for '{identifier}' is null.");
-            }
             if (value is TProperty cast)
             {
                 return cast;
             }
-
             throw new InvalidOperationException($"The value of the '{key}' property for '{identifier}' is an instance of type {typeof(TProperty)}.");
+        }
+
+        public static TProperty GetRequiredProperty<TIdentifier, TProperty>(this IMetadataCollection<TIdentifier> collection, TIdentifier identifier, string key)
+        {
+            var value = collection.GetProperty<TIdentifier, TProperty>(identifier, key);
+            if (value != null)
+            {
+                return value;
+            }
+            throw new InvalidOperationException($"The value of the '{key}' property for '{identifier}' is null.");
         }
     }
 }
