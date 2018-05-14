94ebf406236a4c3c4ac6bcbcd7e5809b6cfbc0c9
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-05-14 16:03:25 +0200

    Remove Lukas' old project

--- DotvvmAcademy/DotvvmAcademy/Resources/_Scripts/Init.ts
+++
@@ -1,8 +0,0 @@
-﻿var dotvvm = (<any>window).dotvvm;
-
-dotvvm.events.init.subscribe(function () {
-    $('.lang-btn').click(function (e) {
-        $('.lang-switcher_list').toggleClass('open'); 
-    });
-});
-
--- DotvvmAcademy/DotvvmAcademy/postcss.config.js
+++
@@ -1,8 +0,0 @@
-﻿module.exports = (context) => ({
-    map: context.options.map,
-    parser: context.options.parser,
-    plugins: [
-        require('./postcss-image-set.js'),
-        require('autoprefixer')({browsers: "last 3 versions"}),
-    ]
-})
