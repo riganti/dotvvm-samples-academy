bc7bd9fcca32bf9387dc16d542e171062ae46e6b
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-05-21 06:52:11 +0200

    Add CodeTaskControl stub

---
+++ src/DotvvmAcademy.Web/Controls/CodeTaskControl.cs
@@ -0,0 +1,36 @@
+﻿using DotVVM.Framework.Binding;
+using DotVVM.Framework.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotvvmAcademy.Web.Controls
+{
+    public class CodeTaskControl : HtmlGenericControl
+    {
+        public string Language
+        {
+            get { return (string)GetValue(LanguageProperty); }
+            set { SetValue(LanguageProperty, value); }
+        }
+
+        public static readonly DotvvmProperty LanguageProperty
+            = DotvvmProperty.Register<string, CodeTaskControl>(c => c.Language, null);
+
+        public string Code
+        {
+            get { return (string)GetValue(CodeProperty); }
+            set { SetValue(CodeProperty, value); }
+        }
+
+        public static readonly DotvvmProperty CodeProperty
+            = DotvvmProperty.Register<string, CodeTaskControl>(c => c.Code, null);
+
+        public CodeTaskControl() : base("div")
+        {
+        }
+
+
+    }
+}
