29bd0334a40b78f8fa3312c3352797ec2d451b9d
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-06-15 10:07:12 +0200

    Add XPathResolvedTreeNavigator stub and dothtml tests project

---
+++ test/DotvvmAcademy.Validation.Dothtml.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotvvmAcademy.Validation.Dothtml.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
