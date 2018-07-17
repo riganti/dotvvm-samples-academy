b1e01de3b7859b8ca52314e15cd79a0532f0cf58
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-07-17 14:29:10 +0200

    Fix bug in .gitignore

---
+++ src/DotvvmAcademy.Meta/WellKnownTypes.cs
@@ -0,0 +1,35 @@
+﻿namespace DotvvmAcademy.Meta
+{
+    public static class WellKnownTypes
+    {
+        public const string Dictionary
+            = "System.Collections.Generic.Dictionary`2";
+
+        public const string IEnumerable
+            = "System.Collections.Generic.IEnumerable`1";
+
+        public const string Int32
+            = "System.Int32";
+
+        public const string KeyValuePair
+            = "System.Collections.Generic.KeyValuePair`2";
+
+        public const string List
+            = "System.Collections.Generic.List`1";
+
+        public const string Object
+            = "System.Object";
+
+        public const string String
+            = "System.String";
+
+        public const string Void
+            = "System.Void";
+
+        public const string DotvvmProperty
+            = "DotVVM.Framework.Binding.DotvvmProperty";
+
+        public const string DotvvmPropertyGroup
+            = "DotVVM.Framework.Compilation.ControlTree";
+    }
+}
