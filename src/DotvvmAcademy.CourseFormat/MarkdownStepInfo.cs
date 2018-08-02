d9e1e01df13bed68556bb7e5b62ad310c89778ea
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-08-02 12:56:07 +0200

    Set the course directory as Content (for MSBuild)

---
+++ src/DotvvmAcademy.CourseFormat/MarkdownStepInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotvvmAcademy.CourseFormat
+{
+    public class MarkdownStepInfo
+    {
+        public string Path { get; }
+
+        public string Moniker { get; }
+
+        public string Name { get; }
+
+        public string CodeTaskPath { get; }
+    }
+}
