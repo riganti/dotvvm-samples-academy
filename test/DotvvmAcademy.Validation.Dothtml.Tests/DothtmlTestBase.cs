5f6ddf3adac77be80d879730766f5487e383b82b
Adam Štěpánek <adam.stepanek@riganti.cz>
2018-08-11 14:39:36 +0200

    Add DothtmlTestBase

---
+++ test/DotvvmAcademy.Validation.Dothtml.Tests/DothtmlTestBase.cs
@@ -0,0 +1,46 @@
+﻿using DotVVM.Framework.Compilation.Parser.Dothtml.Parser;
+using DotVVM.Framework.Compilation.Parser.Dothtml.Tokenizer;
+using DotvvmAcademy.Validation.Dothtml.ValidationTree;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotvvmAcademy.Validation.Dothtml.Tests
+{
+    public abstract class DothtmlTestBase
+    {
+        public DothtmlTestBase()
+        {
+            Provider = BuildServiceProvider();
+        }
+
+        protected IServiceProvider Provider { get; }
+
+        protected virtual IServiceProvider BuildServiceProvider()
+        {
+            var container = new ServiceCollection();
+            RegisterServices(container);
+            return container.BuildServiceProvider();
+        }
+
+        protected virtual ValidationTreeRoot GetValidationTree(string dothtml, string viewModel = null)
+        {
+            var tokenizer = new DothtmlTokenizer();
+            tokenizer.Tokenize(dothtml);
+            var parser = new DothtmlParser();
+            parser.Parse(tokenizer.Tokens);
+        }
+
+        protected virtual void RegisterServices(IServiceCollection container)
+        {
+            container.AddScoped<ValidationControlResolver>();
+            container.AddScoped<ValidationTreeBuilder>();
+            container.AddScoped<ValidationTypeDescriptorFactory>();
+            container.AddScoped<ValidationControlTypeFactory>();
+            container.AddScoped<ValidationTypeDescriptorFactory>();
+            container.AddScoped<ValidationTreeResolver>();
+        }
+    }
+}
---
+++ test/DotvvmAcademy.Validation.Dothtml.Tests/ValidationTreeTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotvvmAcademy.Validation.Dothtml.Tests
+{
+    [TestClass]
+    public class ValidationTreeTests
+    {
+        [TestMethod]
+        public void PropertyGroupsTest()
+        {
+
+        }
+    }
+}
