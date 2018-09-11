97aa5f0cf84f45da51d96766fad16f39d87b2d86
Lukáš Chylík <lukas.chylik@riganti.cz>
2018-09-11 09:02:17 +0200

    lang switcher - outside click fix

--- src/DotvvmAcademy.Web/Resources/_Scripts/LanguageSwitch.ts
+++ src/DotvvmAcademy.Web/Resources/_Scripts/LanguageSwitch.ts
@@ -5,3 +5,10 @@ dotvvm.events.init.subscribe(function () {
         $('.lang-switcher_list').toggleClass('open');
     });
 });
+
+
+$(document).click(function (e) {
+    if ($(e.target).closest($('.lang-switcher')).length == 0) {
+        $('.lang-switcher_list').removeClass('open');
+    }
+})
