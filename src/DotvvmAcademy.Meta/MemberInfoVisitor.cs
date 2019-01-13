bc677ae7300c805975382e9b6ef0e1d8a2ce1337
Adam Štěpánek <adam.stepanek@riganti.cz>
2019-01-13 15:38:55 +0100

    Add MemberInfoVisitor and SymbolVisitor

---
+++ src/DotvvmAcademy.Meta/MemberInfoVisitor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotvvmAcademy.Meta
+{
+    internal class MemberInfoVisitor<TResult>
+    {
+        public virtual TResult Visit(MemberInfo info)
+        {
+            switch(info)
+            {
+                case EventInfo eventInfo:
+                    return VisitEvent(eventInfo);
+                case FieldInfo fieldInfo:
+                    return VisitField(fieldInfo);
+                case PropertyInfo propertyInfo:
+                    return VisitProperty(propertyInfo);
+                case ConstructorInfo constructorInfo:
+                    return VisitConstructor(constructorInfo);
+                case MethodInfo methodInfo:
+                    return VisitMethod(methodInfo);
+                default:
+                    throw new InvalidOperationException($"\"{info.GetType()}\" is not a supported MemberInfo.");
+            }
+        }
+
+        public virtual TResult DefaultVisit(MemberInfo info)
+        {
+            return default;
+        }
+
+        public virtual TResult VisitEvent(EventInfo info)
+        {
+            return DefaultVisit(info);
+        }
+        
+        public virtual TResult VisitField(FieldInfo info)
+        {
+            return DefaultVisit(info);
+        }
+
+        public virtual TResult VisitProperty(PropertyInfo info)
+        {
+            return DefaultVisit(info);
+        }
+
+        public virtual TResult VisitConstructor(ConstructorInfo info)
+        {
+            return DefaultVisit(info);
+        }
+
+        public virtual TResult VisitMethod(MethodInfo info)
+        {
+            return DefaultVisit(info);
+        }
+    }
+}
---
+++ src/DotvvmAcademy.Meta/SymbolVisitor.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotvvmAcademy.Meta
+{
+    internal class SymbolVisitor<TResult>
+    {
+        public virtual TResult Visit(ISymbol symbol)
+        {
+            switch (symbol)
+            {
+                case IAliasSymbol aliasSymbol:
+                    return VisitAlias(aliasSymbol);
+                case IArrayTypeSymbol arrayTypeSymbol:
+                    return VisitArrayType(arrayTypeSymbol);
+                case IAssemblySymbol assemblySymbol:
+                    return VisitAssembly(assemblySymbol);
+                case IDiscardSymbol discardSymbol:
+                    return VisitDiscard(discardSymbol);
+                case IDynamicTypeSymbol dynamicTypeSymbol:
+                    return VisitDynamicType(dynamicTypeSymbol);
+                case IErrorTypeSymbol errorTypeSymbol:
+                    return VisitErrorType(errorTypeSymbol);
+                case IEventSymbol eventSymbol:
+                    return VisitEvent(eventSymbol);
+                case IFieldSymbol fieldSymbol:
+                    return VisitField(fieldSymbol);
+                case ILabelSymbol labelSymbol:
+                    return VisitLabel(labelSymbol);
+                case ILocalSymbol localSymbol:
+                    return VisitLocal(localSymbol);
+                case IMethodSymbol methodSymbol:
+                    return VisitMethod(methodSymbol);
+                case IModuleSymbol moduleSymbol:
+                    return VisitModule(moduleSymbol);
+                case INamedTypeSymbol namedTypeSymbol:
+                    return VisitNamedType(namedTypeSymbol);
+                case INamespaceSymbol namespaceSymbol:
+                    return VisitNamespace(namespaceSymbol);
+                case IParameterSymbol parameterSymbol:
+                    return VisitParameter(parameterSymbol);
+                case IPointerTypeSymbol pointerSymbol:
+                    return VisitPointerType(pointerSymbol);
+                case IPreprocessingSymbol preprocessingSymbol:
+                    return VisitPreprocessing(preprocessingSymbol);
+                case IPropertySymbol propertySymbol:
+                    return VisitProperty(propertySymbol);
+                case IRangeVariableSymbol rangeVariableSymbol:
+                    return VisitRangeVariable(rangeVariableSymbol);
+                case ITypeParameterSymbol typeParameterSymbol:
+                    return VisitTypeParameter(typeParameterSymbol);
+                default:
+                    throw new InvalidOperationException($"\"{symbol.GetType()}\" is not a supported ISymbol.");
+            }
+        }
+
+        public virtual TResult DefaultVisit(ISymbol symbol)
+        {
+            return default;
+        }
+
+        public virtual TResult VisitAlias(IAliasSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitArrayType(IArrayTypeSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitAssembly(IAssemblySymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitDiscard(IDiscardSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitDynamicType(IDynamicTypeSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitErrorType(IErrorTypeSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitEvent(IEventSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitField(IFieldSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitLabel(ILabelSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitLocal(ILocalSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitMethod(IMethodSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitModule(IModuleSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitNamedType(INamedTypeSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitNamespace(INamespaceSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitParameter(IParameterSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitPointerType(IPointerTypeSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitPreprocessing(IPreprocessingSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitProperty(IPropertySymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitRangeVariable(IRangeVariableSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+
+        public virtual TResult VisitTypeParameter(ITypeParameterSymbol symbol)
+        {
+            return DefaultVisit(symbol);
+        }
+    }
+}
