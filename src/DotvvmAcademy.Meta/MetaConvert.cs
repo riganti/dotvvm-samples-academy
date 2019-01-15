fac7a3b8ffbde5979d192591fef96185d65812da
Adam Štěpánek <adam.stepanek@riganti.cz>
2019-01-15 13:34:49 +0100

    Add MetaConvert

---
+++ src/DotvvmAcademy.Meta/MetaConvert.cs
@@ -0,0 +1,46 @@
+﻿using DotvvmAcademy.Meta.Syntax;
+using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace DotvvmAcademy.Meta
+{
+    public static class MetaConvert
+    {
+        public static NameNode ToMeta(ISymbol symbol)
+        {
+            var visitor = new MetaSymbolVisitor();
+            return visitor.Visit(symbol);
+        }
+
+        public static NameNode ToMeta(MemberInfo info)
+        {
+            var visitor = new MetaMemberInfoVisitor();
+            return visitor.Visit(info);
+        }
+
+        public static IEnumerable<MemberInfo> ToReflection(NameNode node, IEnumerable<Assembly> assemblies)
+        {
+            var visitor = new ReflectionNameNodeVisitor(assemblies);
+            return visitor.Visit(node);
+        }
+
+        public static IEnumerable<MemberInfo> ToReflection(ISymbol symbol, IEnumerable<Assembly> assemblies)
+        {
+            var visitor = new ReflectionSymbolVisitor(assemblies);
+            return visitor.Visit(symbol);
+        }
+
+        public static IEnumerable<ISymbol> ToRoslyn(NameNode node, Compilation compilation)
+        {
+            var visitor = new RoslynNameNodeVisitor(compilation);
+            return visitor.Visit(node);
+        }
+
+        public static IEnumerable<ISymbol> ToRoslyn(MemberInfo info, Compilation compilation)
+        {
+            var visitor = new RoslynMemberInfoVisitor(compilation);
+            return visitor.Visit(info);
+        }
+    }
+}
--- src/DotvvmAcademy.Meta/MetaConverter.cs
+++ src/DotvvmAcademy.Meta/MetaConverter.cs
@@ -7,40 +7,51 @@ namespace DotvvmAcademy.Meta
 {
     public class MetaConverter
     {
+        private readonly MetaSymbolVisitor metaSymbolVisitor;
+        private readonly MetaMemberInfoVisitor metaMemberInfoVisitor;
+        private readonly RoslynMemberInfoVisitor roslynMemberInfoVisitor;
+        private readonly RoslynNameNodeVisitor roslynNameNodeVisitor;
+        private readonly ReflectionSymbolVisitor reflectionSymbolVisitor;
+        private readonly ReflectionNameNodeVisitor reflectionNameNodeVisitor;
+
+        public MetaConverter(Compilation compilation, IEnumerable<Assembly> assemblies)
+        {
+            metaSymbolVisitor = new MetaSymbolVisitor();
+            metaMemberInfoVisitor = new MetaMemberInfoVisitor();
+            roslynMemberInfoVisitor = new RoslynMemberInfoVisitor(compilation);
+            roslynNameNodeVisitor = new RoslynNameNodeVisitor(compilation);
+            reflectionSymbolVisitor = new ReflectionSymbolVisitor(assemblies);
+            reflectionNameNodeVisitor = new ReflectionNameNodeVisitor(assemblies);
+        }
+
         public NameNode ToMeta(ISymbol symbol)
         {
-            var visitor = new MetaSymbolVisitor();
-            return visitor.Visit(symbol);
+            return metaSymbolVisitor.Visit(symbol);
         }
 
         public NameNode ToMeta(MemberInfo info)
         {
-            var visitor = new MetaMemberInfoVisitor();
-            return visitor.Visit(info);
+            return metaMemberInfoVisitor.Visit(info);
         }
 
-        public IEnumerable<MemberInfo> ToReflection(NameNode node, IEnumerable<Assembly> assemblies)
+        public IEnumerable<MemberInfo> ToReflection(NameNode node)
         {
-            var visitor = new ReflectionNameNodeVisitor(assemblies);
-            return visitor.Visit(node);
+            return reflectionNameNodeVisitor.Visit(node);
         }
 
-        public IEnumerable<MemberInfo> ToReflection(ISymbol symbol, IEnumerable<Assembly> assemblies)
+        public IEnumerable<MemberInfo> ToReflection(ISymbol symbol)
         {
-            var visitor = new ReflectionSymbolVisitor(assemblies);
-            return visitor.Visit(symbol);
+            return reflectionSymbolVisitor.Visit(symbol);
         }
 
-        public IEnumerable<ISymbol> ToRoslyn(NameNode node, Compilation compilation)
+        public IEnumerable<ISymbol> ToRoslyn(NameNode node)
         {
-            var visitor = new RoslynNameNodeVisitor(compilation);
-            return visitor.Visit(node);
+            return roslynNameNodeVisitor.Visit(node);
         }
 
-        public IEnumerable<ISymbol> ToRoslyn(MemberInfo info, Compilation compilation)
+        public IEnumerable<ISymbol> ToRoslyn(MemberInfo info)
         {
-            var visitor = new RoslynMemberInfoVisitor(compilation);
-            return visitor.Visit(info);
+            return roslynMemberInfoVisitor.Visit(info);
         }
     }
 }
