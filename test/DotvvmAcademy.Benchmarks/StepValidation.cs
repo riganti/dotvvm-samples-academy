50e8ad415946482bc70f00072203623576428a15
Adam Štěpánek <adam.stepanek@riganti.cz>
2019-03-07 17:26:33 +0100

    Optimize obtaining diagnostics from Roslyn

--- test/DotvvmAcademy.Benchmarks/StepValidation.cs
+++ test/DotvvmAcademy.Benchmarks/StepValidation.cs
@@ -3,6 +3,7 @@ using BenchmarkDotNet.Diagnostics.Windows.Configs;
 using DotvvmAcademy.CourseFormat;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,18 @@ namespace DotvvmAcademy.Benchmarks
 {
     public class StepValidation
     {
-        private const string CourseDirectory = @"C:\src\riganti\academy\src\DotvvmAcademy.Web\Course";
+        private const string CourseDirectory = @"C:\src\academy\src\DotvvmAcademy.Web\Course";
 
         [Benchmark]
-        public async Task<int> ValidateContentSample()
+        public async Task ValidateContentSample()
         {
             var workspace = new CourseWorkspace(new FileSystemEnvironment(new DirectoryInfo(CourseDirectory)));
             var step = await workspace.LoadStep("000_content_sample", "en", "10_markdown");
             var diagnostics = await workspace.ValidateStep(step, "");
-            return diagnostics.Count();
+            if (diagnostics.Count() != 3)
+            {
+                throw new InvalidOperationException("There were supposed to be 3 errors.");
+            }
         }
     }
 }
