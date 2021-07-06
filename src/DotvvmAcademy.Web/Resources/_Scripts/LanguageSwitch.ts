e5692296b75a914f733779e10181ef8370d06c35
Lukáš Chylík <lukas.chylik@riganti.cz>
2021-07-06 09:01:58 +0200

    Academy style fixed

--- src/DotvvmAcademy.Web/Resources/_Scripts/LanguageSwitch.ts
+++ src/DotvvmAcademy.Web/Resources/_Scripts/LanguageSwitch.ts
@@ -1,10 +1,10 @@
-﻿let langSwitch = document.querySelector(".lang-switch");
-let langSwitchButon = document.querySelector(".lang-switch__button") as HTMLButtonElement;
-let langSwitchList = document.querySelector(".lang-switch__list") as HTMLDivElement;
+﻿const langSwitch = document.querySelector(".lang-switch");
+const langSwitchButon = document.querySelector(".lang-switch__button") as HTMLButtonElement;
+const langSwitchList = document.querySelector(".lang-switch__list") as HTMLDivElement;
 
-langSwitchButon.addEventListener("click", function () {
+function toggleLanguage() {
     langSwitch.classList.toggle("lang-switch--open");
-});
+}
 
 document.addEventListener("click", function (e) {
     if (!(<HTMLElement>e.target).closest('.lang-switch')) {
