0a3b3f6d160984a681423b825872e528f65f326d
Lukáš Chylík <lukas.chylik@riganti.cz>
2021-07-06 15:22:11 +0200

    navigation fixes, UI fixes

---
+++ src/DotvvmAcademy.Web/Resources/_Scripts/Navigation.ts
@@ -0,0 +1,9 @@
+﻿const navBurger = document.getElementById('showNav') as HTMLButtonElement;
+const navMenu = document.getElementById('navMenu') as HTMLDivElement;
+const body = document.querySelector('body') as HTMLBodyElement;
+
+navBurger.addEventListener('click', function () {
+    navBurger.classList.toggle('open');
+    navMenu.classList.toggle('open');
+    body.classList.toggle('scroll-disabled');
+});
